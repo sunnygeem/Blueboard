c26acc3bc27efbc772d17aeaf4e6acbba384f6af
sheepbelldoor <sheepbelldoor.dev@gmail.com>
2023-10-30 00:56:48 +0900

    modify: firestore connection

--- app/src/main/java/com/se/blueboard/MainActivity.java
+++ app/src/main/java/com/se/blueboard/MainActivity.java
@@ -1,10 +1,26 @@
 package com.se.blueboard;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.Firebase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -12,6 +28,31 @@ public class MainActivity extends AppCompatActivity {
         // main screen (9 buttons)
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Firestore test
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Button firestoreButton = findViewById(R.id.firestore);
+        firestoreButton.setOnClickListener(v -> {
+            // Add a new document with a generated id.
+            Map<String, Object> data = new HashMap<>();
+            data.put("name", "Tokyo");
+            data.put("country", "Japan");
+
+            db.collection("cities")
+                    .add(data)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error adding document", e);
+                        }
+                    });
+        });
 
         // Login button
         Button loginButton = findViewById(R.id.login);
