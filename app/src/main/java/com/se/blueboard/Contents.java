73b68554daf4f6031896f7916e7ab676ce33672c
sheepbelldoor <sheepbelldoor.dev@gmail.com>
2023-11-06 11:33:00 +0900

    modify: lecture contents UI

--- app/src/main/java/com/se/blueboard/Contents.java
+++ app/src/main/java/com/se/blueboard/Contents.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class Contents extends AppCompatActivity {
     @Override
@@ -11,4 +15,20 @@ public class Contents extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contents);
     }
+
+    // Dummy data for recyclerView test
+    ArrayList<String> testDataSet = new ArrayList<>();
+    for(int i = 0; i < 20; i++) {
+        testDataSet.add("test" + i);
+    }
+
+    RecyclerView recyclerView = findViewById(R.id.lectureContents_recyclerView);
+
+    LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+    recyclerView.setLayoutManager(linearLayoutManager);
+
+    CustomAdapter customAdapter = new CustomAdapter(testDataSet);
+
+    recyclerView.setAdapter(customAdapter);
+
 }
