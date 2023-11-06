8a622602cfbafae2168231d6e7c56e650649a0d3
sheepbelldoor <sheepbelldoor.dev@gmail.com>
2023-11-06 17:43:41 +0900

    modify: lecture contents recyclerView

--- app/src/main/java/com/se/blueboard/Contents.java
+++ app/src/main/java/com/se/blueboard/Contents.java
@@ -13,22 +13,21 @@ public class Contents extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.contents);
+        // Dummy data for recyclerView test
+        ArrayList<String> testDataSet = new ArrayList<>();
+        for(int i = 0; i < 20; i++) {
+            testDataSet.add("test" + i);
+        }
+
+        RecyclerView recyclerView = findViewById(R.id.lectureContents_recyclerView);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        CustomAdapter customAdapter = new CustomAdapter(testDataSet);
+
+        recyclerView.setAdapter(customAdapter);
     }
-
-    // Dummy data for recyclerView test
-    ArrayList<String> testDataSet = new ArrayList<>();
-    for(int i = 0; i < 20; i++) {
-        testDataSet.add("test" + i);
-    }
-
-    RecyclerView recyclerView = findViewById(R.id.lectureContents_recyclerView);
-
-    LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-    recyclerView.setLayoutManager(linearLayoutManager);
-
-    CustomAdapter customAdapter = new CustomAdapter(testDataSet);
-
-    recyclerView.setAdapter(customAdapter);
-
 }
