7ed74f8763f5598556dc289fdda8303bc0e1215b
Jiyeon Na <skwldus@hanyang.ac.kr>
2023-11-13 22:02:51 +0900

    feat: add skeleton for Utils

--- app/src/main/java/utils/Utilities.java
+++ app/src/main/java/utils/Utilities.java
@@ -1,4 +1,15 @@
 package utils;
 
 public interface Utilities {
+    boolean isAdmin();
+    boolean fileAvail();
+    boolean lengthAvail(String str, int length);
+    void sortBy();
+    void filterBy();
+    void search(String attribute, String searchString);
+    void gotoPage(String page);
+    void showErrMsg(String errorMessage);
+    void pushAlarm();
+    String masking(String personalInfo);
+    void pagination();
 }
