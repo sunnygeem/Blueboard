ed27b7b16389833063a4abc39884f7d7d2e23436
Jiyeon Na <skwldus@hanyang.ac.kr>
2023-11-13 22:07:37 +0900

    feat: add skeleton for DB

--- app/src/main/java/utils/DB.java
+++ app/src/main/java/utils/DB.java
@@ -1,45 +1,54 @@
 package utils;
 
-public class DB implements Database{
-    public static DB makeDB(){
+public class DB implements Database {
+    public static DB makeDB() {
         // named constructor
         // TODO: implement
         return new DB();
     }
-    public boolean connectDB(){
+
+    public boolean connectDB() {
         // TODO: implement
         return true;
     }
-    public boolean disconnectDB(){
+
+    public boolean disconnectDB() {
         // TODO: implement
         return true;
     }
-    public boolean addData(){
+
+    public boolean addData() {
         // 데이터를 처음 추가할 때
         // TODO: implement
         return true;
     }
-    public boolean updateData(){
+
+    public boolean updateData() {
         // 이미 존재하는 데이터를 업데이트할 때
         // TODO: implement
         return true;
     }
-    public boolean loadData(){
+
+    public boolean loadData() {
         // DB에서 데이터를 가져와서 해당하는 class의 변수를 return
         // TODO: implement
         return true;
     }
-    public boolean deleteData(){
+
+    public boolean deleteData() {
         // TODO: implement
         return true;
     }
-    public boolean uploadFIle(){
+
+    public boolean uploadFile() {
         // firebase의 storage에 파일을 저장하고 그 파일들의 정보를 firestore에 저장
         // TODO: implement
         return true;
     }
-    public boolean downloadFile(){
+
+    public boolean downloadFile() {
         // firebase의 storage에서 파일을 다운로드
         // TODO: implement
         return true;
     }
+}
