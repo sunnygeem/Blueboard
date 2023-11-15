6b72a9dc74f413eb46e8b9b4fc5fd446f142f41c
Jiyeon Na <skwldus@hanyang.ac.kr>
2023-11-15 16:11:33 +0900

    refactor: page class names refactor

--- app/src/main/java/com/se/blueboard/LoginPage.java
+++ app/src/main/java/com/se/blueboard/LoginPage.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Login extends AppCompatActivity {
+public class LoginPage extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
